a5672408c819cce19f8a70988fe796ceb39545bb
kaniskasece <155092917+kaniskasece@users.noreply.github.com>
2025-01-26 10:29:30 +0530

    Add files via upload

---
+++ Fun.js
@@ -0,0 +1,7 @@
+const buttton =document.getElementById("button");
+const funContent = document.getElementById("funContent");
+const fun =document.getElementById("fun");
+
+buttton.onclick()=> {
+    
+}
